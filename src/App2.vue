833bea4514e2a617b69ac655248d6babbe35e2c5
Alain Papazoglou <apapazoglou@leni.fr>
2020-11-12 16:01:50 +0100

    ajout metamask

---
+++ src/App2.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    <h1>Ethereum</h1>
+    <p>network: {{ chainId }}</p>
+  </div>
+</template>
+
+<script>
+/* eslint-disable */
+export default {
+  name: "App",
+  data() {
+    const eth = window.ethereum;
+    if (eth) {
+      eth.enable();
+    } else {
+      alert("Install Metamask");
+    }
+    return { chainId: eth.chainId };
+  }
+};
+</script>
+
+<style>
+main {
+  padding: 1em;
+  max-width: 240px;
+  margin: 0 auto;
+}
+
+h1 {
+  color: #ff3e00;
+  text-transform: uppercase;
+  font-size: 4em;
+  font-weight: 100;
+}
+
+p {
+  font-size: 2em;
+}
+
+@media (min-width: 640px) {
+  main {
+    max-width: none;
+  }
+}
+</style>
